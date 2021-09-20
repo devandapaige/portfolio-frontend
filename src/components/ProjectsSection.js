f975c9babb6a83a29b48d4fcaa19830b9631b36f
Amanda Nelson <amanda.gaylord@gmail.com>
2021-09-19 23:57:11 -0400

    axios GET requested added

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,24 +1,42 @@
-import ProjectCard from "./ProjectCard"
+import ProjectCard from "./ProjectCard";
 import { Swiper } from "swiper/react";
 import "swiper/swiper.min.css";
 import "swiper/components/pagination/pagination.min.css";
 import "swiper/components/navigation/navigation.min.css";
 import SwiperCore, { Pagination, Navigation } from "swiper/core";
+import axios from "axios";
 
 SwiperCore.use([Pagination, Navigation]);
-function ProjectsSection() {
-    const projects = []
-    
 
-    return (
-        <div id="projects">
-            <h2>Projects</h2>
-            <Swiper slidesPerView={3} spaceBetween={30} slidesPerGroup={3} loop={true} loopFillGroupWithBlank={true} pagination={{
-  "clickable": true
-}} navigation={true} className="mySwiper">
-            <ProjectCard />
-            </Swiper>
-        </div>
-    )
+function ProjectsSection() {
+  let projects = [];
+  axios
+    .get("https://amandanelson.herokuapp.com/api/projects/")
+    .then((res) => {
+      projects = res.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  console.log(projects);
+  return (
+    <div id="projects">
+      <h2>Projects</h2>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        slidesPerGroup={3}
+        loop={true}
+        loopFillGroupWithBlank={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        className="mySwiper"
+      >
+        <ProjectCard projects={projects} />
+      </Swiper>
+    </div>
+  );
 }
-export default ProjectsSection
+export default ProjectsSection;
